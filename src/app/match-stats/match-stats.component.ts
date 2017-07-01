c8b8af2c4b86e2fbdd02537648d5e85a3a5bc674
Amitesh <amitesh.gaurav@outlook.com>
2017-07-01 19:30:54 +0530

    added over in live sim

--- src/app/match-stats/match-stats.component.ts
+++ src/app/match-stats/match-stats.component.ts
@@ -15,20 +15,29 @@ export class MatchStatsComponent implements OnInit {
   @Input() matchId: number;
   @Input() statType: string;
   deliveryData: Array<Delivery> = [];
-  matchLength: number = 1;
+  overs: string = "";
+  balls: string = "";
   runs: object = {};
   ballsFaced: object = {};
+  sixes: object = {};
+  fours: object = {};
   bastmenRuns: object = { 1: [], 2: [] };
+  playersdDismissed: Array<String> = [];
+  dismissed: object = {};
+  dismissalMethod: object = {};
+  fielder: object = {};
   team1: string;
   team2: string;
   lastInning = "1";
+  onStrike: string = "";
+
+
   constructor(private _matchDataService: MatchDataService) { }
 
   ngOnInit() {
     let live = this.statType == 'live' ? true : false;
     this._matchDataService.fetchDeliveryData(this.matchId).subscribe((data) => {
       let deliveryIndex = 0;
-      this.matchLength = data.length;
       this.team1 = data[0].batting_team;
       this.team2 = data[0].bowling_team;
       if (live) {
@@ -53,31 +62,65 @@ export class MatchStatsComponent implements OnInit {
   }
 
   doAnalytics(deliveryIndex: number) {
-    //this.tabbedContainer.select(this.deliveryData[deliveryIndex].batting_team);
-    let inning = this.deliveryData[deliveryIndex].inning;
+    let delivery = this.deliveryData[deliveryIndex];
+    this.overs = delivery.over;
+    this.balls = delivery.ball;
+
+    let inning = delivery.inning;
     if (this.lastInning != inning) {
       this.runs = {};
       this.ballsFaced = {};
+      this.fours = {};
+      this.sixes = {};
+      this.dismissed = {};
+      this.dismissalMethod = {};
+      this.fielder = {};
       this.lastInning = inning;
-      this.tabbedContainer["select"](this.deliveryData[deliveryIndex].batting_team);
+      this.tabbedContainer["select"](delivery.batting_team);
     }
 
-    let batsman = this.deliveryData[deliveryIndex].batsman;
+    let batsman = delivery.batsman;
+    this.onStrike = batsman;
 
+    this.dismissed[batsman] = false;
+    this.dismissalMethod[batsman] = delivery.dismissal_kind;
+    this.fielder[batsman] = delivery.fielder;
     if (this.runs.hasOwnProperty(batsman)) {
-      this.runs[batsman] += parseInt(this.deliveryData[deliveryIndex].batsman_runs);
+      this.runs[batsman] += parseInt(delivery.batsman_runs);
       this.ballsFaced[batsman] += 1;
+      if (this.runs[batsman] == 4) {
+        this.fours[batsman]++;
+      }
+      if (this.runs[batsman] == 6) {
+        this.sixes[batsman]++;
+      }
     }
     else {
-      this.runs[batsman] = 0;
-      this.ballsFaced[batsman] = 0;
+      this.runs[batsman] = parseInt(delivery.batsman_runs);
+      this.ballsFaced[batsman] = 1;
+      this.sixes[batsman] = 0;
+      this.fours[batsman] = 0;
+      if (this.runs[batsman] == 4) {
+        this.fours[batsman] = 1;
+      }
+      else if (this.runs[batsman] == 6) {
+        this.sixes[batsman] = 1;
+      }
+
     }
+
+    if (delivery.player_dismissed != "") {
+      this.dismissed[batsman] = true;
+      delivery.dismissal_kind == 'bowled';
+      this.fielder[batsman] = delivery.bowler;
+    }
+
 
 
     this.bastmenRuns[inning] = [];
 
     Object.keys(this.runs).forEach((batsman) => {
-      this.bastmenRuns[inning].push({ name: batsman, runs: this.runs[batsman], ballFaced: this.ballsFaced[batsman] });
+      this.bastmenRuns[inning].push({ name: batsman, runs: this.runs[batsman], ballFaced: this.ballsFaced[batsman], fours: this.fours[batsman], sixes: this.sixes[batsman], dismissed: this.dismissed[batsman], dismissal_kind: this.dismissalMethod[batsman], fielder: this.fielder[batsman] });
     });
 
 
